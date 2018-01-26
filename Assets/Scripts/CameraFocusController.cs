33d4a91c99a8f4dcf12131e13cd810538505d043
David <david94lozoo@gmail.com>
2018-01-26 10:29:30 +0100

    - Fix UI for different resolution - Added timer for deactivation of all the targets - Added CanvasScreenAutoFix and CameraFocusController

---
+++ Assets/Scripts/CameraFocusController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using Vuforia;
+
+public class CameraFocusController : MonoBehaviour
+{
+    private void Start()
+    {
+        var vuforia = VuforiaARController.Instance;
+        vuforia.RegisterVuforiaStartedCallback(OnVuforiaStarted);
+        vuforia.RegisterOnPauseCallback(OnPaused);
+    }
+
+    private void OnVuforiaStarted()
+    {
+        CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+    }
+
+    private void OnPaused(bool paused)
+    {
+        if (!paused) 
+        {
+            // Set again autofocus mode when app is resumed
+            CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+        }
+    }
+}
---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+    private GameObject[] targetArray;
+
+    // Deactive the targets to not start recognition under UI
+	private void Start ()
+    {
+        Invoke("DeactiveGO", .1f);
+	}
+    
+    private void DeactiveGO()
+    {
+        targetArray = GameObject.FindGameObjectsWithTag("Target");
+
+        for (int i = 0; i < targetArray.Length; i++)
+        {
+            targetArray[i].SetActive(false);
+        }
+    }
+}
